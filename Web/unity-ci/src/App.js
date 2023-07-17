5b9684d7c6cc407dd8e5009394eb4db1bacbcd6b
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2023-07-17 11:43:20 +0200

    test react deploy

--- Web/unity-ci/src/App.js
+++ Web/unity-ci/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import React from "react";
+import React, {Fragment} from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "/build/WebGL/WebGL/Build/WebGL.loader.js",
+    dataUrl: "/build/WebGL/WebGL/Build/WebGL.data",
+    frameworkUrl: "/build/WebGL/WebGL/Build/WebGL.framework.js",
+    codeUrl: "/build/WebGL/WebGL/Build/WebGL.wasm",
+  });
 
   return (
-      <h1>Hello World!</h1>
+      <Fragment>
+        {!isLoaded && (
+            <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+        )}
+          <div className={"unity-container"}>
+            <Unity
+                unityProvider={unityProvider}
+                style={{ visibility: isLoaded ? "visible" : "hidden" }}
+            />
+          </div>
+      </Fragment>
   );
 }
 
