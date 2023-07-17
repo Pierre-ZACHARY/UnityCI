2fb081b19cdc1da4daa0e0ea66b2c10973df521f
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2023-07-17 11:52:52 +0200

    build all

--- Web/unity-ci/src/App.js
+++ Web/unity-ci/src/App.js
@@ -3,11 +3,15 @@ import React, {Fragment} from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+    console.log(window.location.pathname);
+
+    const location = window.location.pathname;
+
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: "/build/WebGL/WebGL/Build/WebGL.loader.js",
-    dataUrl: "/build/WebGL/WebGL/Build/WebGL.data",
-    frameworkUrl: "/build/WebGL/WebGL/Build/WebGL.framework.js",
-    codeUrl: "/build/WebGL/WebGL/Build/WebGL.wasm",
+    loaderUrl: location+"/build/WebGL/WebGL/Build/WebGL.loader.js",
+    dataUrl: location+"/build/WebGL/WebGL/Build/WebGL.data",
+    frameworkUrl: location+"/build/WebGL/WebGL/Build/WebGL.framework.js",
+    codeUrl: location+"/build/WebGL/WebGL/Build/WebGL.wasm",
   });
 
   return (
