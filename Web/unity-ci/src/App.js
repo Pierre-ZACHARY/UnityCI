bc4c0818fc3492af9d9f77d62e83e4e834ee60a5
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2023-07-17 10:28:22 +0200

    test react deploy

---
+++ Web/unity-ci/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
+import React, {Fragment} from "react";
+
+function App() {
+
+  return (
+      <h1>Hello World!</h1>
+  );
+}
+
+export default App;
