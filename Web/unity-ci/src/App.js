1e5fce8bd540b29c1384617f0f57755b29bf2a3b
Zachary Pierre <pierre.zachary@etu.univ-orleans.fr>
2023-07-22 17:52:30 +0200

    build from zip

--- Web/unity-ci/src/App.js
+++ Web/unity-ci/src/App.js
@@ -1,32 +1,67 @@
 import './App.css';
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
-
+import JSZip from "jszip";
+import { inflate } from "pako";
+import {} from "js-untar";
 function App() {
     console.log(window.location.pathname);
 
-    const location = window.location.pathname;
+    const [urls, setUrls] = React.useState({loaderUrl: "", dataUrl: "", wasmUrl: "", frameworkUrl: ""});
+    useEffect(()=>{
+        const asyncFetch = async () => {
+            console.log("Fetching The build...");
+            const response = await fetch("https://github.com/Pierre-ZACHARY/UnityCI/suites/14483264822/artifacts/818433185", {mode: "no-cors"});
+            console.log("Extracting...");
+            const data = await response.arrayBuffer();
+            const zip = await JSZip.loadAsync(data);
+            const loaderJs = zip.file("WebGL/WebGL/Build/WebGL.loader.js");
+            const dataGz = zip.file("WebGL/WebGL/Build/WebGL.data.gz");
+            const wasmGz = zip.file("WebGL/WebGL/Build/WebGL.wasm.gz");
+            const frameworkJsGz = zip.file("WebGL/WebGL/Build/WebGL.framework.js.gz");
+            if(!(dataGz && wasmGz && loaderJs && frameworkJsGz)){
+                console.error("Missing files in WebGLBuild.zip");
+                return;
+            }
+            const dataGzArray = await dataGz.async("arraybuffer");
+            const wasmGzArray = await wasmGz.async("arraybuffer");
+            const frameworkJsGzArray = await frameworkJsGz.async("arraybuffer");
+            const loaderJsArray = await loaderJs.async("arraybuffer");
+            const tarDataGzArray = inflate(new Uint8Array(dataGzArray), { to: 'array'});
+            const tarWasmGzArray = inflate(new Uint8Array(wasmGzArray), { to: 'array'});
+            const tarFrameworkJsGzArray = inflate(new Uint8Array(frameworkJsGzArray), { to: 'array'});
+            const loaderBlob = new Blob([loaderJsArray], {type: "application/javascript"});
+            const dataBlob = new Blob([tarDataGzArray], {type: "application/data"});
+            const wasmBlob = new Blob([tarWasmGzArray], {type: "application/wasm"});
+            const frameworkBlob = new Blob([tarFrameworkJsGzArray], {type: "application/javascript"});
+            console.log("Extraction successful!");
 
-  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-    loaderUrl: location+"/build/WebGL/WebGL/Build/WebGL.loader.js",
-    dataUrl: location+"/build/WebGL/WebGL/Build/WebGL.data",
-    frameworkUrl: location+"/build/WebGL/WebGL/Build/WebGL.framework.js",
-    codeUrl: location+"/build/WebGL/WebGL/Build/WebGL.wasm",
-  });
+            const dataUrl = URL.createObjectURL(dataBlob);
+            const wasmUrl = URL.createObjectURL(wasmBlob);
+            const frameworkUrl = URL.createObjectURL(frameworkBlob);
+            const loaderUrl = URL.createObjectURL(loaderBlob);
 
-  return (
-      <Fragment>
-        {!isLoaded && (
-            <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
-        )}
-          <div className={"unity-container"}>
-            <Unity
-                unityProvider={unityProvider}
-                style={{ visibility: isLoaded ? "visible" : "hidden" }}
-            />
-          </div>
-      </Fragment>
-  );
+            setUrls({loaderUrl: loaderUrl, dataUrl: dataUrl, wasmUrl: wasmUrl, frameworkUrl: frameworkUrl});
+        };
+        asyncFetch();
+    }, []);
+
+
+    return (<>
+        { urls.loaderUrl.length>0 ? <DisplayUnity {...urls} /> : <div>Loading...</div> }
+    </>);
+}
+
+function DisplayUnity({loaderUrl, dataUrl, frameworkUrl, wasmUrl}){
+    console.log("Loading Unity...");
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/WebGL.loader.js",
+        dataUrl: dataUrl,
+        frameworkUrl: frameworkUrl,
+        codeUrl: wasmUrl,
+    });
+
+    return <Unity unityProvider={unityProvider} />;
 }
 
 export default App;
