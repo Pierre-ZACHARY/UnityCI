256d9a6853b06fded36dae385b350f5610be9f45
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2023-08-01 17:18:28 +0200

    Initial commit

---
+++ Assets/Editor/BuildIosSimulator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public class BuildIosSimulator
+    {
+        static void BuildiOS()
+        {
+            // Get all scenes added to the build settings
+            EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+
+            // Create an array to store scene paths
+            string[] scenePaths = new string[scenes.Length];
+
+            // Extract the scene paths from EditorBuildSettingsScene array
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                scenePaths[i] = scenes[i].path;
+            }
+
+            Debug.Log("Building iOS Simulator");
+            Debug.Log(scenePaths);
+            // Set the output folder for the iOS build
+            string outputFolder = "Builds/iOS";
+            BuildTarget buildTarget = BuildTarget.iOS;
+
+            // set sdk to iphone simulator
+            PlayerSettings.iOS.sdkVersion = iOSSdkVersion.SimulatorSDK;
+            // Perform iOS build
+            BuildPipeline.BuildPlayer(scenes, outputFolder, buildTarget, BuildOptions.None);
+        }
+    }
+}
---
+++ ios_simulator.dockerfile
@@ -0,0 +1,20 @@
+﻿FROM unityci/editor:ubuntu-2023.1.3f1-ios-1.1.2
+
+WORKDIR /app
+
+# --build-arg UNITY_LICENSE="$UNITY_LICENSE"
+ARG UNITY_LICENSE
+RUN echo $UNITY_LICENSE > UnityLicense.ulf
+
+COPY ./Assets ./Assets
+COPY ./Packages ./Packages
+COPY ./ProjectSettings ./ProjectSettings
+
+# Activate Unity
+#RUN /opt/unity/Editor/Unity -batchmode -manualLicenseFile UnityLicense.ulf -logfile -
+# Generate Unity Project
+#RUN /opt/unity/Editor/Unity -batchmode -createProject /app -logfile -
+# Execute Build Ios Simulator which is a custom editor script, output to Builds/iOS
+#RUN /opt/unity/Editor/Unity -batchmode -projectPath /app -executeMethod Editor.BuildIosSimulator.BuildiOS -logfile 
+
+#RUN ls -la /app/Builds/iOS
