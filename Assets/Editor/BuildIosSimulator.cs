87b6a575545658e6fc4b6da7f3e1d38ea1fc44d7
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2023-08-03 10:00:01 +0200

    Update BuildIosSimulator.cs

--- Assets/Editor/BuildIosSimulator.cs
+++ Assets/Editor/BuildIosSimulator.cs
@@ -8,27 +8,27 @@ namespace Editor
         static void BuildiOS()
         {
             // Get all scenes added to the build settings
-            EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+            //EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
 
             // Create an array to store scene paths
-            string[] scenePaths = new string[scenes.Length];
+            //string[] scenePaths = new string[scenes.Length];
 
             // Extract the scene paths from EditorBuildSettingsScene array
-            for (int i = 0; i < scenes.Length; i++)
-            {
-                scenePaths[i] = scenes[i].path;
-            }
+            //for (int i = 0; i < scenes.Length; i++)
+            //{
+            //    scenePaths[i] = scenes[i].path;
+            //}
 
-            Debug.Log("Building iOS Simulator");
-            Debug.Log(scenePaths);
+            //Debug.Log("Building iOS Simulator");
+            //Debug.Log(scenePaths);
             // Set the output folder for the iOS build
-            string outputFolder = "Builds/iOS";
-            BuildTarget buildTarget = BuildTarget.iOS;
+            //string outputFolder = "Builds/iOS";
+            //BuildTarget buildTarget = BuildTarget.iOS;
 
             // set sdk to iphone simulator
-            PlayerSettings.iOS.sdkVersion = iOSSdkVersion.SimulatorSDK;
+            //PlayerSettings.iOS.sdkVersion = iOSSdkVersion.SimulatorSDK;
             // Perform iOS build
-            BuildPipeline.BuildPlayer(scenes, outputFolder, buildTarget, BuildOptions.None);
+            //BuildPipeline.BuildPlayer(scenes, outputFolder, buildTarget, BuildOptions.None);
         }
     }
 }
